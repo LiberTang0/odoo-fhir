ffd895c8380ee2d75a3d1065a5cf916c17d63d27
Luigi Sison <lsison@moxylus.com>
2016-05-30 09:12:58 -0700

    Changes

--- addons/hc_base/models/models.py
+++ addons/hc_base/models/models.py
@@ -56,7 +56,7 @@ class CountryRegion(models.Model):
     _inherit = ["hc.value.set.contains"]
 
     name = fields.Char(string="Region", help="A grouping of divisions (e.g. West and Midwest in the US).")
-    type_id = fields.Many2one(comodel_name="hc.vs.country.division.type", string="Division Type", help="Type of grouping of divisions (e.g. Region in the US).")
+    type_id = fields.Many2one(comodel_name="hc.vs.country.division.type", string="Region Type", help="Type of grouping of divisions (e.g. Region in the US).")
     country_id = fields.Many2one(comodel_name="res.country", string="Country", help="Country (can be ISO-3166 3-letter code).")
 
 class CountryDivisionType(models.Model):    
@@ -129,6 +129,14 @@ class CountryCityType(models.Model):
     name = fields.Char(string="City/Place Type", help="Type of place or district/county subdivision (e.g., city, town).")
     country_id = fields.Many2one(comodel_name="res.country", string="Country", help="Country that maintains the city/place type.")
 
+class CountryCityCategory(models.Model):    
+    _name = "hc.vs.country.city.category"    
+    _description = "City/Place Category"       
+    _inherit = ["hc.value.set.contains"]
+
+    name = fields.Char(string="City/Place Category", help="Category of place or district/county subdivision (e.g., census designated place, incorporated place).")
+    country_id = fields.Many2one(comodel_name="res.country", string="Country", help="Country that maintains the city/place category.")
+
 class CountryCityStatus(models.Model):    
     _name = "hc.vs.country.city.status"    
     _description = "City/Place Functional Status"       
@@ -139,11 +147,13 @@ class CountryCityStatus(models.Model):
 
 class CountryCity(models.Model):    
     _name = "hc.vs.country.city"    
-    _description = "City"       
+    _description = "City/Place"       
     _inherit = ["hc.value.set.contains"]
 
     name = fields.Char(string="City/Place", help="The name of the city, town, village or other community or delivery center.")
+    numeric_code = fields.Char(string="Numeric Code", help="Numeric code that uniquely identifies a place within a primary subdivision (e.g. In US, 5-digit FIPS Place Code).")
     type_id = fields.Many2one(comodel_name="hc.vs.country.city.type", string="Place Type", help="Type of place or county subdivision (e.g., city, town).")
+    category_id = fields.Many2one(comodel_name="hc.vs.country.city.category", string="Place Category", help="Category of place or district/county subdivision (e.g., census designated place, incorporated place).")
     status_id = fields.Many2one(comodel_name="hc.vs.country.city.status", string="Functional Status", help="Status of a place from a legal and/or statistical perspective.")
     postal_code_ids = fields.Many2many(comodel_name="hc.vs.country.postal.code", string="Postal Codes", help="A group of numbers or letters and numbers that are added to a postal address to assist the sorting of mail.")
     primary_postal_code_city = fields.Boolean(string="Primary City", help="Indicates if this city is the primary city for a postal code.")
@@ -211,10 +221,12 @@ class Address(models.Model):
     region_id = fields.Many2one(comodel_name="hc.vs.country.region", string="Region", help="A grouping of divisions or states (e.g. West, Midwest).")
     country_id = fields.Many2one(comodel_name="res.country", string="Country", help="Country (can be ISO-3166 3-letter code).")
 
+
 class HcExtensionAddress(models.Model):
     _inherit = 'hc.address'
 
     postal_code_id = fields.Many2one(comodel_name="hc.vs.country.postal.code", string="Postal Code", help="Postal code for area.")
+    
     division_id = fields.Many2one(related='postal_code_id.division_id', string="Division", help="Group of primary subdivisions (e.g., Pacific and Mountain in the US).")
     region_id = fields.Many2one(related='postal_code_id.region_id', string="Region", help="A grouping of divisions or states (e.g. West, Midwest).")
     country_id = fields.Many2one(related='postal_code_id.country_id', string="Country", help="Country (can be ISO-3166 3-letter code).")
@@ -309,56 +321,108 @@ class Attachment(models.Model):
     hash_attachment = fields.Binary(string="Hash", help="Hash of the data (sha-1, base64ed ).")
     creation_date = fields.Datetime(string="Attachment Creation Date", help="Date attachment was first created.")
 
-class SuffixHumanName(models.Model):    
-    _name = "hc.vs.suffix.human.name"   
-    _description = "Suffix Human Name"      
-    _inherit = ["hc.value.set.contains"]     
-
 class HumanNameTerm(models.Model):  
-    _name = "hc.vs.human.name.term" 
-    _description = "Human Name Term"        
+    _name = "hc.human.name.term" 
+    _description = "Human Name Term"
+    _inherit = ["res.partner.title"]         
+
+    name = fields.Char( string="Human Name Term", help="Term part of a human name (e.g., John, Smith).")
+
+class SuffixHumanName(models.Model):    
+    _name = "hc.suffix.human.name"   
+    _description = "Suffix Human Name"      
+    _inherit = ["res.partner.title"]     
+
+    name = fields.Char( string="Suffix Human Name Term", help="Suffix part of a human name. May be a family title (e.g., Jr.) or initials of a credential (e.g., RN).")
 
 class HumanName(models.Model):
 
     _name = "hc.human.name"
     _description = "Human Name"
 
+    # use = fields.Selection(string="Human Name Use", 
+    #     selection=[
+    #         ("usual", "Usual"), 
+    #         ("official", "Official"),
+    #         ("temp", "Temp"),
+    #         ("nickname", "Nickname"),
+    #         ("anonymous", "Anonymous"),
+    #         ("old", "Old"),
+    #         ("maiden", "Maiden")],
+    #     help="The use of a human name.")
+    
     use = fields.Selection(string="Human Name Use", 
         selection=[
             ("usual", "Usual"), 
             ("official", "Official"),
             ("temp", "Temp"),
-            ("nickname", "Nickname"),
             ("anonymous", "Anonymous"),
-            ("old", "Old"),
-            ("maiden", "Maiden")],
+            ("old", "Old")],
         help="The use of a human name.")
-    name = fields.Char(string="Full Name", help="A full text representation of the human name.")
+    name = fields.Char(string="Full Name", readonly=True, help="A full text representation of the human name.")
     family_ids = fields.One2many(comodel_name="hc.human.name.family", inverse_name="human_name_id", string="Family Names", help="Family name (often called 'Surname').")
-    given_ids = fields.One2many(comodel_name="hc.human.name.given", inverse_name="human_name_id", string="Given Names", help="Given names (not always 'first'). Includes middle names.")
-    prefix_ids = fields.One2many(comodel_name="hc.human.name.prefix", inverse_name="human_name_id", string="Prefix Names", help="Parts that come before the name.")
-    suffix_ids = fields.One2many(comodel_name="hc.human.name.suffix", inverse_name="human_name_id", string="Suffix Names", help="Parts that come after the name.")
+    # given_ids = fields.One2many(comodel_name="hc.human.name.given", inverse_name="human_name_id", string="Given Names", help="Given names (not always 'first'). Includes middle names.")
+    prefix_ids = fields.Many2many(comodel_name="res.partner.title", string="Prefix Names", help="Parts that come before the name.")
+    first_id = fields.Many2one(comodel_name="hc.human.name.term", string="First Name", help="Part of given name.")
+    middle_ids = fields.Many2many(comodel_name="hc.human.name.term", string="Middle Names", help="Part of given name.")
+    initial_ids = fields.Many2many(comodel_name="hc.human.name.term", string="Initial Names", help="Part of given name.")
+    nickname_ids = fields.Many2many(comodel_name="hc.human.name.term", string="Nickname", help="Part of given name.")
+    surname_id = fields.Many2one(comodel_name="hc.human.name.term", string="Surname", help="Part of last name.")
+    previous_last_id = fields.Many2one(comodel_name="hc.human.name.term", string="Previous Surname", help="Part of last name.")
+    suffix_ids = fields.Many2many(comodel_name="hc.suffix.human.name", string="Suffix Names", help="Parts that come after the name.")
     preferred_name = fields.Char(string="Preferred Name", help="How the person prefers to be named.")
-    mother_maiden_family_name = fields.Char(string="Mother Maiden Family Name", help="Mother's original family name.")
-    birth_family_name = fields.Char(string="Birth Family Name", help="Person's family name at birth.")
+    mother_maiden_family_name_id = fields.Many2one(comodel_name="hc.human.name.term", string="Mother Maiden Family Name", help="Mother's original family name.")
+    birth_family_name_id = fields.Many2one(comodel_name="hc.human.name.term", string="Birth Family Name", help="Person's family name at birth.")
     is_last_before_first = fields.Boolean(string="Last Before First", help="Indicates that family name(s) are arranged before given name(s). Example: Chinese names.")
     is_maiden_after_last = fields.Boolean(string="Maiden After Last", help="Indicates that last name(s) are arranged before maiden name(s). Example: Spanish names.")
+
+class HcExtensionHumanName(models.Model):
+    _inherit = 'hc.human.name'
+
+    @api.model
+    def create(self, vals):
+
+        first = self.env['hc.human.name.term'].browse(vals['first_id']).name
+        last = self.env['hc.human.name.term'].browse(vals['surname_id']).name
+        full = first+' '+last
+        vals['name'] = full
+
+        return super(HcExtensionHumanName, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+       
+
+        if 'first_id' in vals:   
+            first = self.env['hc.human.name.term'].browse(vals['first_id']).name
+        else:
+            first = self.first_id.name
+
+        if 'surname_id' in vals:    
+            last = self.env['hc.human.name.term'].browse(vals['surname_id']).name
+        else:
+            last = self.surname_id.name
+
+        full = first+' '+last
+        vals['name'] = full
+
+        return super(HcExtensionHumanName, self).create(vals)
 
 class HumanNameGiven(models.Model): 
     _name = "hc.human.name.given"   
     _description = "Human Name Given"
 
     human_name_id = fields.Many2one(comodel_name="hc.human.name", string="Human Name", help="Human name associated with this given name.")
-    name_id = fields.Many2one(comodel_name="hc.vs.human.name.term", string="Name", help="Given name of this human name.")
+    name_id = fields.Many2one(comodel_name="hc.human.name.term", string="Name", help="Given name of this human name.")
     sequence = fields.Integer(string="Sequence", default="1", size=1, help="Display order of this given name.")
-    name_type = fields.Selection(string="Type", selection=[("first", "First"), ("middle", "Middle"), ("initial", "Initial"), ("nickname", "Nickname")], help="Type of given name.")
+    type = fields.Selection(string="Type", selection=[("first", "First"), ("middle", "Middle"), ("initial", "Initial"), ("nickname", "Nickname")], help="Type of given name.")
 
 class HumanNameFamily(models.Model):    
     _name = "hc.human.name.family"  
     _description = "Human Name Family"
 
     human_name_id = fields.Many2one(comodel_name="hc.human.name", string="Human Name", help="Human name associated with this family name.")
-    name_id = fields.Many2one(comodel_name="hc.vs.human.name.term", string="Name", help="Family name of this human name.")
+    name_id = fields.Many2one(comodel_name="hc.human.name.term", string="Name", help="Family name of this human name.")
     sequence = fields.Integer(string="Sequence", default="1", size=1, help="Display order of this family name.")
     name_type = fields.Selection(string="Type", selection=[("surname", "Surname"), ("patronymic", "Patronymic"), ("previous (maiden)", "Previous (Maiden)"), ("mother", "Mother")], help="Type of family name.")
 
@@ -374,7 +438,7 @@ class HumanNameSuffix(models.Model):
     _description = "Human Name Suffix"      
 
     human_name_id = fields.Many2one(comodel_name="hc.human.name", string="Human Name", help="Human name associated with this suffix name.")
-    suffix_id = fields.Many2one(comodel_name="hc.vs.suffix.human.name", string="Suffix", help="Suffix name of this human name.")
+    suffix_id = fields.Many2one(comodel_name="hc.suffix.human.name", string="Suffix", help="Suffix part of a human name. May be a family title (e.g., Jr.) or initials of a credential (e.g., RN).")
 
 class IdentifierType(models.Model): 
     _name = "hc.vs.identifier.type" 
