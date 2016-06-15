719c26e5d3d352d4acf1c2a1421af34af7aa49c2
Luigi Sison <lsison@moxylus.com>
2016-06-15 13:46:21 -0700

    Changes

--- addons/hc_base/models/hc_human_name.py
+++ addons/hc_base/models/hc_human_name.py
@@ -7,20 +7,24 @@ class HumanNameTerm(models.Model):
     _description = "Human Name Term"
     _inherit = ["res.partner.title"]         
 
-    name = fields.Char( string="Human Name Term", help="Term part of a human name (e.g., John, Smith).")
+    name = fields.Char(
+        string="Human Name Term", 
+        help="A single term (part) of a human name (e.g., John, Smith).")
 
     _sql_constraints = [
-        ('name_unique',
-        'UNIQUE(name)',
-        "The term must be unique."),
+        ("name_unique",
+        "UNIQUE(name)",
+        "The term must be unique.")
         ]
 
 class SuffixHumanName(models.Model):    
-    _name = "hc.suffix.human.name"   
-    _description = "Suffix Human Name"      
+    _name = "hc.human.name.suffix"   
+    _description = "Human Name Suffix"      
     _inherit = ["res.partner.title"]     
 
-    name = fields.Char( string="Suffix Human Name Term", help="Suffix part of a human name. May be a family title (e.g., Jr.) or initials of a credential (e.g., RN).")
+    name = fields.Char(
+        string="Suffix Human Name Term", 
+        help="Parts that come after the name. Aka post-nominal letters. May be a family title (e.g., Jr.) a credential (e.g., RN), a title (.e.g., OBE), or a degree (e.g., PhD).")
 
 class HumanName(models.Model):
 
@@ -36,23 +40,122 @@ class HumanName(models.Model):
             ("anonymous", "Anonymous"),
             ("old", "Old"),
             ("maiden", "Maiden")],
+        default="usual",
         help="The use of a human name.")
-    name = fields.Char(string="Full Name", readonly=True, help="A full text representation of the human name.")
-    # family_ids = fields.One2many(comodel_name="hc.human.name.family", inverse_name="human_name_id", string="Family Names", help="Family name (often called 'Surname').")
-    # given_ids = fields.One2many(comodel_name="hc.human.name.given", inverse_name="human_name_id", string="Given Names", help="Given names (not always 'first'). Includes middle names.")
-    prefix_ids = fields.Many2many(comodel_name="res.partner.title", string="Prefix Names", help="Parts that come before the name.")
-    first_id = fields.Many2one(comodel_name="hc.human.name.term", string="First Name", help="Part of given name.")
-    middle_ids = fields.Many2many(comodel_name="hc.human.name.term", relation="middle_name_human_term_rel", string="Middle Names", help="Part of given name.")
-    initial_ids = fields.Many2many(comodel_name="hc.human.name.term", relation="initial_name_human_term_rel", string="Initial Names", help="Part of given name.")
-    nickname_ids = fields.Many2many(comodel_name="hc.human.name.term", relation="nick_name_human_term_rel", string="Nickname", help="Part of given name.")
-    surname_id = fields.Many2one(comodel_name="hc.human.name.term", string="Surname", help="Part of last name.")
-    previous_last_id = fields.Many2one(comodel_name="hc.human.name.term", string="Previous Surname", help="Part of last name.")
-    suffix_ids = fields.Many2many(comodel_name="hc.suffix.human.name", string="Suffix Names", help="Parts that come after the name.")
-    preferred_name = fields.Char(string="Preferred Name", help="How the person prefers to be named.")
-    mother_maiden_name_id = fields.Many2one(comodel_name="hc.human.name.term", string="Mother Maiden Name", help="Mother's family name at birth.")
-    birth_last_name_id = fields.Many2one(comodel_name="hc.human.name.term", string="Birth Last Name", help="Person's family name at birth.")
-    is_last_before_first = fields.Boolean(string="Last Before First", default=True, help="Indicates that family name(s) are arranged before given name(s). Example: Chinese names.")
-    is_maiden_after_last = fields.Boolean(string="Maiden After Last", default=False, help="Indicates that last name(s) are arranged before maiden name(s). Example: Spanish names.")
+    name = fields.Char(
+        store=True,
+        string="Full Name",
+        help="A full text representation of the human name.")
+    given = fields.Char(
+        store=True,
+        string="Given Name", 
+        readonly=True,
+        help="Given names (not always 'first'). Includes middle names.")
+    family = fields.Char(
+        store=True,
+        string="Family Name", 
+        readonly=True,
+        help="The parts of a name that links to the genealogy. (e.g., surname, birth last name).")
+    prefix_ids = fields.Many2many(
+        comodel_name="res.partner.title", 
+        string="Prefix Names", 
+        help="Parts that come before the name.")
+    first_id = fields.Many2one(
+        comodel_name="hc.human.name.term", 
+        string="First Name", 
+        help="Part of given name.")
+    middle_ids = fields.Many2many(
+        comodel_name="hc.human.name.term", 
+        relation="middle_name_human_term_rel", 
+        string="Middle Names", 
+        help="Part of given name.")
+    initial_ids = fields.Many2many(
+        comodel_name="hc.human.name.term", 
+        relation="initial_name_human_term_rel", 
+        string="Initial Names", 
+        help="Part of given name.")
+    nickname_ids = fields.Many2many(
+        comodel_name="hc.human.name.term", 
+        relation="nickname_human_term_rel", 
+        string="Nicknames", 
+        help="Part of given name.")
+    surname_id = fields.Many2one(
+        comodel_name="hc.human.name.term", 
+        string="Surname", 
+        help="Part of family name.")
+    previous_last_id = fields.Many2one(
+        comodel_name="hc.human.name.term", 
+        string="Previous Surname", 
+        help="Part of family name (e.g., previous married family name).")
+    suffix_ids = fields.Many2many(
+        comodel_name="hc.human.name.suffix", 
+        string="Suffix Names", 
+        help="Parts that come after the name.")
+    preferred_name = fields.Char(
+        string="Preferred Name", 
+        help="How the person prefers to be addressed in a conversation (e.g., John, Mr. Smith).")
+    mother_maiden_name_id = fields.Many2one(
+        comodel_name="hc.human.name.term", 
+        string="Mother Maiden Name", 
+        help="Mother's surname at birth. Part of the family name.")
+    birth_last_name_id = fields.Many2one(
+        comodel_name="hc.human.name.term", 
+        string="Birth Last Name", 
+        help="Person's surname at birth.")
+    display_order = fields.Selection(string="Display Name Order", 
+        selection=[
+            ("given maiden last", "Given Maiden Last (e.g., American name)"), 
+            ("maiden last first", "Maiden Last First (e.g., Chinese name)"),
+            ("first last maiden", "First Last Maiden (e.g., Spanish name)")],
+        default="given maiden last",
+        help="The display order of this human name.")
+
+# class HcExtensionHumanNameFull(models.Model):
+#     _inherit = 'hc.human.name'
+
+#     @api.depends('first_id','surname_id')
+#     def compute_full(self):
+#         full = ''
+#         lines = ''
+#         first = self.first_id and ', '+self.first_id.name or ''
+#         surname = self.surname_id and ', '+self.surname_id.name or ''
+#         lines = first+surname+lines
+#         self.name = lines
+
+#     name = fields.Char(
+#         compute='compute_full', 
+#         store=True,
+#         string="Full Name",
+#         help="A full text representation of the human name.")
+
+# class HcExtensionHumanNameFamily(models.Model):
+#     _inherit = 'hc.human.name'
+
+#     @api.depends('mother_maiden_name_id','surname_id')
+#     def compute_family(self):
+#         family = ''
+#         lines = ''
+#         maiden = self.mother_maiden_name_id and ', '+self.mother_maiden_name_id.name or ''
+#         surname = self.surname_id and ', '+self.surname_id.name or ''
+#         lines = maiden+surname+lines
+#         self.name = lines
+
+#     family = fields.Char(
+#         compute="compute_family",
+#         store=True,
+#         string="Family Name", 
+#         help="Family name (often called 'Surname').")
+
+# Requirements
+
+# No mandatory fields
+
+# compute: Given = First Name + Middle Names + Initial Names + "(" + Nicknames +")"
+# compute: Family = Mother Maiden Name + Birth Name + Previous Name + Last Name
+# compute: Family_Reverse = Birth Name + Previous Name + Last Name + Mother Maiden Name
+# compute: Full = Prefix + Given + Family + Suffix
+# compute: Full_Reverse = Prefix + Family + Given + Suffix
+# compute: Full_Family_Reverse = Prefix + Given + Family_Reverse + Suffix
 
 class HcExtensionHumanName(models.Model):
     _inherit = 'hc.human.name'
@@ -61,21 +164,35 @@ class HcExtensionHumanName(models.Model):
     def create(self, vals):
 
         first = self.env['hc.human.name.term'].browse(vals['first_id']).name
+        # middle = self.env['hc.human.name.term'].browse(vals['middle_ids']).name
         last = self.env['hc.human.name.term'].browse(vals['surname_id']).name
         maiden = self.env['hc.human.name.term'].browse(vals['mother_maiden_name_id']).name
         full = first+' '+last
+        # full_first = first+' '+middle
+        full_family = maiden+' '+last
         vals['name'] = full
+        # vals['given'] = full_first
+        vals['family'] = full_family
 
         return super(HcExtensionHumanName, self).create(vals)
 
     @api.multi
     def write(self, vals):
        
-
         if 'first_id' in vals:   
             first = self.env['hc.human.name.term'].browse(vals['first_id']).name
         else:
             first = self.first_id.name
+
+        # if 'middle_ids' in vals:   
+        #     middle = self.env['hc.human.name.term'].browse(vals['middle_ids']).name
+        # else:
+        #     middle = self.middle_ids.name
+
+        if 'mother_maiden_name_id' in vals:    
+            maiden = self.env['hc.human.name.term'].browse(vals['mother_maiden_name_id']).name
+        else:
+            maiden = self.mother_maiden_name_id.name    
 
         if 'surname_id' in vals:    
             last = self.env['hc.human.name.term'].browse(vals['surname_id']).name
@@ -83,6 +200,10 @@ class HcExtensionHumanName(models.Model):
             last = self.surname_id.name
 
         full = first+' '+last
+        # full_first = first+' '+middle
+        full_family = maiden+' '+last
         vals['name'] = full
+        # vals['given'] = full_first
+        vals['family'] = full_family
 
         return super(HcExtensionHumanName, self).create(vals)
