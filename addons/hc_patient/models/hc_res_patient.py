0fc991b3f2d3dbcba8b04f6440f63fb28de512c3
Luigi Sison <lsison@moxylus.com>
2016-06-15 17:14:32 -0700

    Added hc_patient

--- addons/hc_patient/models/hc_res_patient.py
+++ addons/hc_patient/models/hc_res_patient.py
@@ -5,22 +5,69 @@ from openerp import models, fields, api
 class Patient(models.Model):    
     _name = "hc.res.patient"    
     _description = "Patient"
-    _inherit = ["hc.res.person"]
+    _inherits = {"hc.res.person": "person_id"}
 
-    is_deceased = fields.Boolean(string="Deceased", help="Indicates if the patient is deceased or not.")
-    deceased_date = fields.Datetime(string="Deceased Date", help="When the patient was deceased.")
-    marital_status_id = fields.Many2one(comodel_name="hc.vs.marital.status", string="Marital Status", help="Marital (civil) status of a patient.")
+    person_id = fields.Many2one(
+        comodel_name="hc.res.person",
+        string="Person",
+        required=True,
+        ondelete="restrict",
+        help="Person who is this patient.")
+    birth_time = fields.Char(string="Birth Time", help="The time when the patient was born.")
+    is_deceased = fields.Boolean(string="Deceased", default=False, help="Indicates if the patient is deceased or not.")
+    deceased_date = fields.Date(string="Deceased Date", help="The date when the patient died.")
+    deceased_time = fields.Char(string="Deceased Time", help="The time when the patient died.")
+    marital_status_ids = fields.One2many(
+        comodel_name="hc.patient.marital.status",
+        inverse_name="patient_id", 
+        string="Marital Statuses", 
+        help="Marital (civil) status of a person.")
+    race_ids = fields.Many2many(
+        comodel_name="hc.vs.race", 
+        relation="race_patient_rel", 
+        string="Races", 
+        help="General race category reported by the patient - subject may have more than one.")
+    ethnicity_ids = fields.Many2many(
+        comodel_name="hc.vs.ethnicity", 
+        relation="ethnicity_patient_rel", 
+        string="Ethnicities", 
+        help="General ethnicity category reported by the patient - subject may have more than one.")
     is_multiple_birth = fields.Boolean(string="Multiple Birth", help="Whether patient is part of a multiple birth.")
     multiple_birth_count = fields.Integer(string="Multiple Birth Count", size=1, help="Number of births in a multiple birth.")
     multiple_birth_order = fields.Integer(string="Multiple Birth Order", size=1, help="The actual birth order in a multiple birth.")
     # care_provider_practitioner_ids = fields.One2many(comodel_name="hc.res.practitioner", inverse_name="patient_id", string="Care Provider Practitioners", help="Practitioner who is patient's nominated care provider.")
     # care_provider_organization_ids = fields.One2many(comodel_name="hc.res.organization", inverse_name="patient_id", string="Care Provider Organizations", help="Organization who is patient's nominated care provider.")
     # managing_organization_id = fields.Many2one(comodel_name="hc.res.organization", string="Managing Organization", help="Organization that is the custodian of the patient record.")
-    is_active = fields.Boolean(string="Active", help="Whether this patient's record is in active use.")
+    is_active_patient = fields.Boolean(string="Active Patient", default=True, help="Whether this patient's record is in active use.")
+    is_patient = fields.Boolean(string="Patient", default=True, help="Indicates a patient record.")
 
 class MaritalStatus(models.Model):  
     _name = "hc.vs.marital.status"  
     _description = "Marital Status" 
+    _inherit = ["hc.value.set.contains"]
+
+class PatientMaritalStatus(models.Model):   
+    _name = "hc.patient.marital.status"  
+    _description = "Patient Marital Status"
+    _inherit = ["hc.basic.association"]
+
+    patient_id = fields.Many2one(
+        comodel_name="hc.res.patient", 
+        string="Patient", 
+        help="Patient associated with this marital status.")
+    marital_status_id = fields.Many2one(
+        comodel_name="hc.vs.marital.status", 
+        string="Marital Status", 
+        help="Marital Status associated with this patient.")
+
+class Race(models.Model):  
+    _name = "hc.vs.race"  
+    _description = "Race" 
+    _inherit = ["hc.value.set.contains"]
+
+class Ethnicity(models.Model):  
+    _name = "hc.vs.ethnicity"  
+    _description = "Ethnicity" 
     _inherit = ["hc.value.set.contains"]
 
 class PatientContact(models.Model): 
